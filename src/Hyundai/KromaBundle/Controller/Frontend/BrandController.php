d8865550fdc2444c4e33838044df8c44cbb5c9e3
BigBoss <damian.damico86@gmail.com>
2014-02-21 12:56:23 -0300

    Download resource

--- src/Hyundai/KromaBundle/Controller/Frontend/BrandController.php
+++ src/Hyundai/KromaBundle/Controller/Frontend/BrandController.php
@@ -3,6 +3,7 @@
 namespace Hyundai\KromaBundle\Controller\Frontend;
 
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Symfony\Component\HttpFoundation\Response;
 
 class BrandController extends ResourceController
 {
@@ -31,5 +32,22 @@ class BrandController extends ResourceController
         	$config->getResourceName() => $brand,
         	'resourcesCategories' => $resourcesCategories
         ));
+    }
+    
+    public function downloadAction($filename)
+    {
+    	$request = $this->get('request');
+    	$path = $this->get('kernel')->getRootDir(). "/../web/media/resources/";
+    	$content = file_get_contents($path.$filename);
+    	
+    	$response = new Response();
+    	
+    	//set headers
+    	$response->headers->set('Content-Type', 'mime/type');
+    	$response->headers->set('Content-Disposition', 'attachment;filename="'.$filename);
+    	
+    	$response->setContent($content);
+    	
+    	return $response;
     }
 }
